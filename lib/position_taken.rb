aefb691df7e176c1abd86e60f0f994bcbf6413ef
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-27 02:12:58 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@
 def position_taken?(board, char)
   if (board[char] == " " || board[char] == "" || board[char] == nil)
     false
+  elsif (board[char] == "X" || board[char] == "O"
+    true
   end
 end
